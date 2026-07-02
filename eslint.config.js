c2b9cde95c7a3d073b167ee3da6bae5dc8589b28
FailedMaster <failedmaster666@gmail.com>
2026-07-02 02:53:49 +0200

    added eslint

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from "@eslint/js";
+import globals from "globals";
+import json from "@eslint/json";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      // Modify the default unused variables rule
+      "no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "^\\$\\$?$",
+          argsIgnorePattern: "^\\$\\$?$",
+        },
+      ],
+      "no-console": "warn",
+    },
+  },
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.jsonc"],
+    plugins: { json },
+    language: "json/jsonc",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+  },
+]);
--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
-import './style.css'
+import "./style.css";
 
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-$('#app').innerHTML = `
+$("#app").innerHTML = `
   <div>
     <h1>Hello Finkenroulette!</h1>
     <img src="/mysterybeer.png"/ style="width: 300px; height: 300px;"/>
   </div>
 `;
 
-$('img').addEventListener('click', () => {
-  $('#app').insertAdjacentHTML('beforeend', '<span style="font-size: 8em;">🍺</span>');
+$("img").addEventListener("click", () => {
+  $("#app").insertAdjacentHTML(
+    "beforeend",
+    '<span style="font-size: 8em;">🍺</span>',
+  );
 });
